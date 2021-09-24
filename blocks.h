0f70d063e7313464a32fdbea9d9100b8c4e01a5e
lbrayner <lbrayner@users.noreply.github.com>
2021-09-24 14:26:10 -0300

    internet

---
+++ blocks.h
@@ -0,0 +1,11 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "$HOME/.local/share/dwm/blocks/internet",	10,		0},
+
+	/* {"", "date '+%b %d (%a) %I:%M%p'",					5,		0}, */
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " ";
+static unsigned int delimLen = 1;
